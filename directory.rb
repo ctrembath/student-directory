39c616972e29dd9f639adffa315f9007cd30b0d8
clare trembath <claretrembath@ymail.com>
2014-10-25 14:09:50 +0100

    program for entering students names

--- directory.rb
+++ directory.rb
@@ -1,26 +1,39 @@
-student_count = 19
-#first we print the lst of students
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-puts "Ben Hutchinson"
-puts "Rich Lake"
-puts "Chris Handley"
-puts "Izzy Markwick"
-puts "Nick Dyer"
-puts "Alex Blease"
-puts "Giogia Amici"
-puts "Christopher Batts"
-puts "Dale Stevens"
-puts "Edward Byne"
-puts "Alan Bridger"
-puts "Tim Scully"
-puts "Mishal L"
-puts "Pavel Redics"
-puts "Simon Lange"
-puts "Clare Trembath"
-puts "Claudia B"
-puts "Ayo Obembe"
-puts "Benjam Conway"
-#finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+def print_header
+		puts "The students of my cohort at Makers Academy"
+		puts "---------------------"
+	end
+
+def print(students)
+		students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	end
+end
+
+def print_footer(names)
+		puts "Overall, we have #{names.length} great students"
+end
+	#putting students in arra
+	def input_students
+		puts "Please enter the names of the students"
+		puts "To finish just hit return twice"
+		#create an empty array
+		students = []
+		#get the first name
+		name = gets.chomp
+		#while the name is not empty repeat this code
+		while !name.empty? do
+			#add the stuent hash to the array
+		students << {:name => name, :cohort => :october}
+		puts "Now we have #{students.length} students"
+		#get another name from the user
+		name = gets.chomp
+		end
+		#return the array of students
+		students
+
+	end
+	students = input_students
+	print_header
+	print(students)
+	print_footer(students)
 
